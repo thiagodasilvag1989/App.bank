3dfb1b9a5e02dde0a722b811d2c763413bb2de0e
Thiago da Silva <thiagodasilvag1989@gmail.com>
2021-04-09 18:41:53 -0300

    App Bank

---
+++ App.Bank/Enums/TipoConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Bank
+{
+    public enum TipoConta {
+        PessoaFisica = 1,
+        PessoaJuridira = 2
+    }
+    
+}
